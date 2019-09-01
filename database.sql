d8bf7e623fc958cb044e1c1263041a6578808313
danilozag1992 <danilozagarcanin@gmail.com>
2019-09-01 19:05:20 +0200

    added sql queries

---
+++ database.sql
@@ -0,0 +1,138 @@
+-- Checking a list The word IN allows us to check if an item is in a list. The example shows the name and population for the countries 'Brazil', 'Russia', 'India' and 'China'.
+-- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+
+
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+-- Which countries are not too small and not too big? BETWEEN allows range checking (range specified is inclusive of boundary values). The example below shows countries with an area of 250,000-300,000 sq. km. Modify it to show the country and the area for countries with an area between 200,000 and 250,000.
+
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000;
+
+-- The example uses a WHERE clause to show the population of 'France'. Note that strings (pieces of text that are data) should be in 'single quotes';
+-- Modify it to show the population of Germany
+
+SELECT population FROM world
+  WHERE name = 'Germany';
+
+-- Change the query shown so that it displays Nobel prizes for 1950.
+
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950;
+
+-- Show who won the 1962 prize for Literature.
+
+SELECT winner
+
+
+FROM nobel
+
+WHERE yr = 1962
+
+AND subject = 'Literature';
+
+
+-- Show the year and subject that won 'Albert Einstein' his prize.
+
+
+ SELECT yr, subject
+
+
+FROM nobel
+
+WHERE winner = 'Albert Einstein';
+-- Give the name of the 'Peace' winners since the year 2000, including 2000.
+SELECT winner
+FROM nobel
+WHERE subject = 'Peace'
+AND yr >= 2000;
+
+-- Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+
+SELECT yr, subject, winner
+
+
+FROM nobel
+
+WHERE subject = 'Literature'
+
+AND yr BETWEEN 1980 AND 1989;
+
+-- Show all details of the presidential winners:
+--     • Theodore Roosevelt
+--     • Woodrow Wilson
+--     • Jimmy Carter
+--     • Barack Obama
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter', 'Barack Obama');
+
+-- Show the winners with first name John
+
+
+SELECT winner
+
+
+FROM nobel
+
+WHERE winner LIKE 'John%';
+
+
+-- Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
+
+SELECT yr, subject, winner
+
+
+FROM nobel
+
+WHERE (subject = 'Physics' AND yr = 1980) OR (subject = 'Chemistry' AND yr = 1984);
+-- Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
+
+
+SELECT yr, subject, winner
+
+
+FROM nobel
+
+WHERE yr = 1980 AND subject <> 'Chemistry' AND subject <> 'Medicine';
+
+-- Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+
+SELECT yr, subject, winner
+
+
+FROM nobel
+
+WHERE (subject = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004);
+Find all details of the prize won by PETER GRÜNBERG
+SELECT *
+FROM nobel
+WHERE winner LIKE 'PETER GR_NBERG';
+
+
+-- Knights in order
+-- List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
+
+SELECT winner, yr, subject
+
+
+FROM nobel
+
+WHERE winner LIKE 'Sir%'
+
+ORDER BY yr DESC, winner;
+
+-- The expression subject IN ('Chemistry','Physics') can be used as a value - it will be 0 or 1.
+-- Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+
+SELECT winner, subject
+
+
+FROM nobel
+
+WHERE yr=1984
+
+ORDER BY subject IN ('Physics', 'Chemistry'), subject, winner;
